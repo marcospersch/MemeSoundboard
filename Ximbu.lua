bbf462d64c4836141a4c789f72f0dbbb0b64ad8d
Marcos Persch <marcosv.p@hotmail.com>
2019-04-01 16:42:17 -0300

    Update Ximbu.lua

--- Ximbu.lua
+++ Ximbu.lua
@@ -1,5 +1,26 @@
-MainMenuBarArtFrame.LeftEndCap:Hide()
-MainMenuBarArtFrame.RightEndCap:Hide()
-local b = QuickJoinToastButton b:UnregisterAllEvents() b:SetScript("OnShow", b.Hide) b:Hide() UIParent:UnregisterEvent("UNIT_AURA")
-QuickJoinToastButton.Toast2:Hide()
-QuickJoinToastButton.Toast:Hide()
+
+function Ximbu()
+	HideGryphon();
+	HideSocialButton();
+end
+
+function HideGryphon()
+	HideGryphonLeft();
+	HideGryphonRight();
+end
+
+function HideGryphonLeft()
+	MainMenuBarArtFrame.LeftEndCap:Hide()
+end
+
+function HideGryphonRight()
+	MainMenuBarArtFrame.RightEndCap:Hide()
+end
+
+function HideSocialButton(self)
+	local b = QuickJoinToastButton b:UnregisterAllEvents() b:SetScript("OnShow", b.Hide) b:Hide() UIParent:UnregisterEvent("UNIT_AURA")
+	QuickJoinToastButton.Toast2:Hide()
+	QuickJoinToastButton.Toast:Hide()
+end
+
+Ximbu();
